09d01d15446ffb607f63e77dcfe8b1ad5ed5f2ab
Norimitsu <norimitsu@Norimitsus-iMac.local>
2025-11-24 13:00:35 +0900

    fix(cp3): mock menu master patch upsert

--- functions/menu/master/[item_id].ts
+++ functions/menu/master/[item_id].ts
@@ -7,12 +7,18 @@ export const onRequestPatch: PagesFunction<Env> = async ({ env, params, request 
   const body = await request.json().catch(() => ({} as any));
 
   if (isMock(env)) {
-    const i = (store.menu || []).findIndex((x:any)=>x.item_id===item_id);
-    if (i < 0) return bad(404, "ITEM_NOT_FOUND", "item not found");
-    store.menu[i] = { ...store.menu[i], ...body, item_id };
+    store.menu = store.menu || [];
+    let i = store.menu.findIndex((x:any)=>x.item_id===item_id);
+
+    if (i < 0) {
+      // upsert create
+      store.menu.push({ item_id, ...body });
+      i = store.menu.length - 1;
+    } else {
+      store.menu[i] = { ...store.menu[i], ...body, item_id };
+    }
     return json(store.menu[i]);
   }
 
-  // real: TODO PATCH MENU_MASTER
   return bad(501, "REAL_API_NOT_IMPLEMENTED", "real api not implemented");
 };
